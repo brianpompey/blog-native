accf4c73fda0fa29bb83911c44e60ad565745abb
brianpompey <brianthescholar@gmail.com>
2022-02-02 14:21:42 -0500

    need to redo the whole project i think

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack'
 import React from 'react';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 import IndexScreen from './src/screens/indexScreen';
 import { BlogProvider } from './src/context/BlogContext';
 
